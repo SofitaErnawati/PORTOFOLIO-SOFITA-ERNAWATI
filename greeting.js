8c2aeecbdd38fb0ed1c01d04306ed06a1562a958
SofitaErnawati <sofitaernawati700@gmail.com>
2025-01-06 00:49:53 +0700

    Add files via upload

---
+++ greeting.js
@@ -0,0 +1,26 @@
+// greeting.js atau untuk meegucapkan salam sesuai dengan suasana
+document.addEventListener('DOMContentLoaded', () => {
+    const preTitle = document.querySelector('.pre-title');
+    
+    function updateGreeting() {
+        const hour = new Date().getHours();
+        let greeting;
+        
+        if (hour >= 3 && hour < 11) {
+            greeting = "Selamat Pagi";
+        } else if (hour >= 11 && hour < 15) {
+            greeting = "Selamat Siang";
+        } else if (hour >= 15 && hour < 18) {
+            greeting = "Selamat Sore";
+        } else {
+            greeting = "Selamat Malam";
+        }
+        
+        if (preTitle) {
+            preTitle.textContent = `${greeting}, Selamat Datang Di Website Portofolio Saya`;
+        }
+    }
+    
+    updateGreeting();
+    setInterval(updateGreeting, 60000);
+});
